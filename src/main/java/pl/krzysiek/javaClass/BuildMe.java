7a56e3c7b72c04979d3ff45c2749b4c9a13a13af
KS-coder-98 <krzysztofsekowski1998@gmail.com>
2025-01-08 00:49:42 +0100

    add comments to Builder pattern

--- src/main/java/pl/krzysiek/javaClass/BuildMe.java
+++ src/main/java/pl/krzysiek/javaClass/BuildMe.java
@@ -2,13 +2,23 @@ package pl.krzysiek.javaClass;
 
 import java.time.LocalDate;
 
+/**
+ * <p>
+ *     <ul>
+ *         <li>By making the BuildMe constructor private, you ensure that instances can only be created through the Builder.</li>
+ *         <li>Nullifying the template in the build() method ensures that the Builder cannot be reused to modify an already created BuildMe instance, maintaining immutability after the object is built.</li>
+ *     </ul>
+ * </p>
+ */
 public class BuildMe {
     private String name;
     private LocalDate date;
 
+    // Private constructor ensures BuildMe instances can only be created via the Builder
     private BuildMe() {
     }
 
+    // Static method to create a new Builder instance
     public static Builder builder() {
         return new Builder();
     }
@@ -21,26 +31,29 @@ public class BuildMe {
                 '}';
     }
 
+    // Builder class for creating and customizing BuildMe instances
     public static class Builder {
         private BuildMe template = new BuildMe();
 
+        // Private constructor to restrict instantiation outside the enclosing class
         private Builder() {
         }
 
         public Builder name(String name) {
             template.name = name;
-            return this;
+            return this; // Enables chaining of method calls
         }
 
         public Builder date(LocalDate date) {
             template.date = date;
-            return this;
+            return this; // Enables chaining of method calls
         }
 
+        // Finalizes the BuildMe instance and prevents further modification
         public BuildMe build() {
-            BuildMe rv = template;
-            template = null; //don't modify later
-            return rv;
+            BuildMe rv = template; // Assign the current template to the return value
+            template = null; //don't modify later. Nullify the template to prevent reuse or modification
+            return rv; // Return the finalized instance
         }
     }
 }
