581827e7dfdc735b3cc33147a5c505936f3e20d1
KS-coder-98 <krzysztofsekowski1998@gmail.com>
2025-01-19 20:18:52 +0100

    add InstanceMethods

---
+++ src/main/java/pl/krzysiek/definingClassContents/InstanceMethods.java
@@ -0,0 +1,49 @@
+package pl.krzysiek.definingClassContents;
+
+/**
+ * <ul>
+ *     <li>
+ *         Do not have the static modifier.
+ *         Non-static methods are associated with an instance of a class and cannot be marked as static.
+ *         This means they must be invoked on a specific instance of the class.
+ *     </li>
+ *     <li>
+ *         The firs (usually implicit) argument is
+ *         <ul>
+ *             <li>
+ *                 called this -> it represents the object on which the method is invoked
+ *             </li>
+ *             <li>
+ *                 It is of the enclosing type (the type of the class in which the method is defined)
+ *             </li>
+ *             <li>
+ *                 It is "effectively final" – meaning the reference this cannot be reassigned within the method.
+ *             </li>
+ *         </ul>
+ *     </li>
+ *     <li>
+ *         Must be invoked with an instance of the enclosing type
+ *         Invoking a non-static method requires a reference to an instance of the class. This can happen in the following ways:
+ *         <ul>
+ *             <li>
+ *                 Implicitly through the this value – within the same class, the method can be invoked directly without specifying the object.
+ *             </li>
+ *             <li>
+ *                 Explicitly through the this value – by explicitly using this, for example, this.methodName().
+ *             </li>
+ *             <li>
+ *                 Any non-null expression referring to an instance – by using any valid reference to an instance of the class, such as objectInstance.methodName().
+ *             </li>
+ *         </ul>
+ *     </li>
+ * </ul>
+ * <h3>
+ *     Summary
+ * </h3>
+ * <p>
+ *     In summary, non-static methods are tied to specific instances of a class, use the this reference for context,
+ *     and must be invoked on an appropriate object. Let me know if you'd like further clarification!
+ * </p>
+ */
+public class InstanceMethods {
+}
